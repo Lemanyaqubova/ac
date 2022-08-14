c4b9001c0fcab94d6ecb7f91f416e45704eb2622
Lemanyaqubova <lamanny@code.edu.az>
2022-08-14 21:59:45 +0400

    file

---
+++ ConsoleApp1/ConsoleApp1/Models/Singer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    class Singer
+    {
+        //fiedls
+        private string _name;
+        private string _surname;
+        private int _age;
+
+        //props
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length < 101)
+                {
+                    _name = value;
+                }
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (value.Length < 101)
+                {
+                    _surname = value;
+                }
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value<171)
+                {
+                    _age = value;
+                }
+            }
+
+        }
+    }
+}
